14c3b74bf90ff6c6cdd609fcb13abbc80515200f
Phillips <sphillips@northwestfcu.org>
2016-02-03 11:14:08 -0500

    scp - partexcel - happy state for report file to excel

--- partdatatocsv/partexcel.pl
+++ partdatatocsv/partexcel.pl
@@ -15,17 +15,14 @@ GetOptions (	"d=s" => \$datafile,
 		"r=s" => \$reportfile,
 		"i=s" => \$interestfile,
 		"a=s" => \$dateappend);
-	
 
-##########################################
-###   Parsing Section
-##########################################
-#probably will move this to a module
-#should read everything
 
 my $parser = Parsing::LoanParticipant->new();
 my %excelfiles;
 
+###
+#Datafile Section
+###
 if($datafile){
 	open my $infile, "<", $datafile or die $!;
 	my @infile = <$infile>;
@@ -74,7 +71,54 @@ for my $part (keys %parts) {
 use warnings "uninitialized";
 }
 
-
+###
+#Reportfile Section
+###
+if($reportfile){
+	open my $infile, "<", $reportfile or die $!;
+	my @infile = <$infile>;
+	close $infile;
+	my %parts = $parser->ParseReportFile(@infile);
+	
+	#we want to be able to add '' to a number and not get a warning
+	no warnings 'numeric';
+	for my $part (keys %parts) {
+		my $outfile =  "$part". "$dateappend\.xls";
+		$excelfiles{$outfile} = $excelfiles{$outfile} // Spreadsheet::WriteExcel->new($outfile) or die $!;
+		my $sheet = $excelfiles{$outfile}->add_worksheet('Statement Data from Report');
+		my $row = 7;
+		$sheet->write($row,0, ["Date","Sequence",'Participation','Agreement ID','Action Code','Principal','Interest','Service Fee','Running Balance']);
+		my ($principal, $interest, $late, $service) = (0,0,0,0);
+				
+		for my $partrec (@{$parts{$part}}){
+			#some data reformatting:
+			map { $partrec->{$_} =~ s/([\d\.]+)-/-$1/ } keys %$partrec;
+			$row++;
+			$sheet->write($row,0,[$partrec->{'Date'},$partrec->{'SequenceNumber'},$partrec->{'Participation'},$partrec->{'ID'},$partrec->{'Code'},$partrec->{'Principal'},$partrec->{'Interest'},$partrec->{'Fees'},$partrec->{'Balance'}]);
+			$principal += $partrec->{'Principal'};
+			$interest += $partrec->{'Interest'};
+			$service += $partrec->{'Fees'};
+		}
+		#Draw the key:
+			$sheet->write($row+3, 4, [['A - Loan Addon',
+		'B - Fund Agr',
+		'D - Remove Agr',
+		'F - Remove Ln',
+		'G - Fund Adj',
+		'H - LnPmt Adj',
+		'I - Pmt Recd',
+		'J - Pmt Disb',
+		'P - LnPmt Dist']]);
+			#sums
+			$sheet->write(1,0, ['Principal:', $principal]);
+			$sheet->write(2,0, ['Interest:', $interest]);
+			$sheet->write(3,0, ['Late Charges:', $late]);
+			$sheet->write(4,0, ['Service Fee', $service]);
+		
+	}
+	use warnings 'numeric';
+	
+}
 
 #close the excel spreadsheets - technically going out of scope should call this, but not bad practice
 for my $excelfile (keys %excelfiles){
